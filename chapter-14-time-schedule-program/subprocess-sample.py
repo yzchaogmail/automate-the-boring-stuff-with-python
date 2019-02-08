ca13a5365ac466c631e1d6e865fc5347b4fecf18
yzchaogmail <yzchao@gmail.com>
2019-02-08 17:31:53 +0800

    subproces/popen

---
+++ chapter-14-time-schedule-program/subprocess-sample.py
@@ -0,0 +1,14 @@
+#! python3
+# -*- coding:utf-8 -*-
+
+import os,sys,logging
+import subprocess as sp
+
+#sp.Popen(['notepad.exe','hello.txt'])
+logging.basicConfig(filename='debuglog.txt',level=logging.DEBUG,format='%(asctime)s-%(levelname)s-%(message)s')
+
+sp.Popen('notepad.exe')
+sp.Popen(['notepad.exe','temp.txt'])
+logging.debug('before----------------')
+sp.Popen(['start','start-hell0.txt'],shell=True)
+logging.debug('after----------------')
