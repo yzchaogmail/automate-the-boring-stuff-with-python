792c78fdcd3075eec0790b89379b6e4f73629718
yzchaogmail <yzchao@gmail.com>
2019-02-10 00:50:23 +0800

    DONE string-sample.py

---
+++ chapter-1-6-basic-learning/string-sample.py
@@ -0,0 +1,21 @@
+#! python3
+# -*- coding:utf-8 -*-
+import pyperclip,sys
+str = ''
+str.upper()
+str.lower()
+str.isupper()
+str.islower()
+str.isalpha()
+str.isdigit()
+str.isalnum()
+str.startswith('')
+str.endswith('')
+str.join([])
+str.split()
+str.rjust(1)
+str.ljust(2)
+str.center(3)
+str.strip()
+str.lstrip()
+str.rstrip()
